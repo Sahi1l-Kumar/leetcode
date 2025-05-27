af29eea55a13cd5842b484276102c7dfb27d349b
Sahi1l-Kumar <2230189@kiit.ac.in>
2025-05-27 20:50:42 +0530

    Day 8

---
+++ Arrays/Medium/15_3Sum.java
@@ -0,0 +1,68 @@
+/**
+ * Problem: 4. Median of Two Sorted Arrays
+ * Difficulty: Hard
+ * Link: https://leetcode.com/problems/median-of-two-sorted-arrays/
+ *
+ * Description:
+ * Given two sorted arrays nums1 and nums2 of size m and n respectively, return
+ * the median of the two sorted arrays.
+ * The overall run time complexity should be O(log (m+n)).
+ *
+ * Example 1:
+ * 
+ * Input: nums = [-1,0,1,2,-1,-4]
+ * Output: [[-1,-1,2],[-1,0,1]]
+ * Explanation:
+ * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+ * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+ * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+ * The distinct triplets are [-1,0,1] and [-1,-1,2].
+ * Notice that the order of the output and the order of the triplets does not
+ * matter.
+ * 
+ * Example 2:
+ * 
+ * Input: nums = [0,1,1]
+ * Output: []
+ * Explanation: The only possible triplet does not sum up to 0.
+ * 
+ * Example 3:
+ * 
+ * Input: nums = [0,0,0]
+ * Output: [[0,0,0]]
+ * Explanation: The only possible triplet sums up to 0.
+ *
+ * 
+ * Constraints:
+ * nums1.length == m
+ * nums2.length == n
+ */
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int k = nums.length - 1;
+
+            for (int j = i + 1; j < k;) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    while (nums[j] == nums[j - 1] && j < k)
+                        j++;
+                }
+            }
+        }
+        return result;
+    }
+}
