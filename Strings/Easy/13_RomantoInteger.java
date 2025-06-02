7783415313264ce5b7af0c7d0e409692f3cfc127
Sahi1l-Kumar <2230189@kiit.ac.in>
2025-06-02 21:24:16 +0530

    Day 13

---
+++ Strings/Easy/13_RomantoInteger.java
@@ -0,0 +1,110 @@
+/**
+ * Problem: 13. Roman to Integer
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/roman-to-integer/
+ *
+ * Description:
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D
+ * and M.
+ * Symbol Value
+ * I 1
+ * V 5
+ * X 10
+ * L 50
+ * C 100
+ * D 500
+ * M 1000
+ * For example, 2 is written as II in Roman numeral, just two ones added
+ * together. 12 is written as XII, which is simply X + II. The number 27 is
+ * written as XXVII, which is XX + V + II.
+ * Roman numerals are usually written largest to smallest from left to right.
+ * However, the numeral for four is not IIII. Instead, the number four is
+ * written as IV. Because the one is before the five we subtract it making four.
+ * The same principle applies to the number nine, which is written as IX. There
+ * are six instances where subtraction is used:
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given a roman numeral, convert it to an integer.
+ *
+ * Example 1:
+ * Input: s = "III"
+ * Output: 3
+ * Explanation: III = 3.
+ *
+ * Example 2:
+ * Input: s = "LVIII"
+ * Output: 58
+ * Explanation: L = 50, V= 5, III = 3.
+ *
+ * Example 3:
+ * Input: s = "MCMXCIV"
+ * Output: 1994
+ * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ *
+ */
+
+// Solution 1
+
+class Solution {
+    public int romanToInt(String s) {
+        int ans = 0, num = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            switch (s.charAt(i)) {
+                case 'I':
+                    num = 1;
+                    break;
+                case 'V':
+                    num = 5;
+                    break;
+                case 'X':
+                    num = 10;
+                    break;
+                case 'L':
+                    num = 50;
+                    break;
+                case 'C':
+                    num = 100;
+                    break;
+                case 'D':
+                    num = 500;
+                    break;
+                case 'M':
+                    num = 1000;
+                    break;
+            }
+            if (4 * num < ans)
+                ans -= num;
+            else
+                ans += num;
+        }
+        return ans;
+
+    }
+}
+
+// Solution 2 (HashMap)
+
+class Solution {
+    public int romanToInt(String s) {
+        int res = 0;
+        Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
+                res -= roman.get(s.charAt(i));
+            } else {
+                res += roman.get(s.charAt(i));
+            }
+        }
+
+        return res + roman.get(s.charAt(s.length() - 1));
+    }
+}
